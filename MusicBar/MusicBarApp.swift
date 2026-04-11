ced095621a857eb9b4106afa3eea8561178ac908
Oron Paz <oron.paz26@gmail.com>
2026-04-11 21:29:29 +0300

    Initial Commit

---
+++ MusicBar/MusicBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBarApp.swift
+//  MusicBar
+//
+//  Created by Oron Paz on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
